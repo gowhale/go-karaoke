33797db4fe6480aa3a5a8daf7422759f1384d30b
gowhale <gabewhale@gmail.com>
2023-01-08 16:26:41 +0000

    fix(lint): action all lint feedback

--- main.go
+++ main.go
@@ -20,8 +20,6 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	startTime := time.Now()
-
 	startTimes := []int{}
 	for _, lyric := range lyrics.Lyrics {
 		startTimes = append(startTimes, lyric.StartTime)
@@ -50,29 +48,47 @@ func main() {
 		}
 	}()
 
-	scrn.AllLEDSOff()
+	if err := startKaraoke(scrn, lyrics, cfg, startTimes); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func startKaraoke(scrn gui.Screen, lyrics config.LyricsConfig, cfg gc.PinConfig, startTimes []int) error {
+	startTime := time.Now()
+	if err := scrn.AllLEDSOff(); err != nil {
+		return err
+	}
 	count := 0
 	for count < len(startTimes)-1 {
 		nextStartTime := (time.Duration(startTimes[count]) * time.Second).Seconds()
 		currentTime := time.Duration(time.Since(startTime)).Seconds()
 		if nextStartTime < currentTime {
-			displayTime := startTimes[count+1] - startTimes[count]
-			// log.Println(currentTime, displayTime, lyrics.Lyrics[count])
-
-			lowercaseString := strings.ToLower(lyrics.Lyrics[count].Line)
-			lowercaseString = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(lowercaseString, "")
-			matrixLyric, err := matrix.ConcatanateLetters(lowercaseString)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			matrixLyric, err = matrix.TrimMatrix(matrixLyric, cfg, 0)
-			if err != nil {
-				log.Fatalln(err)
+			if err := displayLyric(scrn, lyrics, cfg, startTimes, count); err != nil {
+				return err
 			}
 
-			scrn.DisplayMatrix(matrixLyric, time.Second*time.Duration(displayTime))
-			scrn.AllLEDSOff()
 			count++
 		}
 	}
+	return nil
+}
+
+func displayLyric(scrn gui.Screen, lyrics config.LyricsConfig, cfg gc.PinConfig, startTimes []int, count int) error {
+	displayTime := startTimes[count+1] - startTimes[count]
+
+	lowercaseString := strings.ToLower(lyrics.Lyrics[count].Line)
+	lowercaseString = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(lowercaseString, "")
+	matrixLyric, err := matrix.ConcatanateLetters(lowercaseString)
+	if err != nil {
+		return err
+	}
+	matrixLyric, err = matrix.TrimMatrix(matrixLyric, cfg, 0)
+	if err != nil {
+		return err
+	}
+
+	if err := scrn.DisplayMatrix(matrixLyric, time.Second*time.Duration(displayTime)); err != nil {
+		return err
+	}
+	return scrn.AllLEDSOff()
 }
--- pkg/config/lyrics.go
+++ pkg/config/lyrics.go
@@ -1,36 +1,69 @@
+// Package config deals with the loading of lyric files
 package config
 
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
 
+// Lyric contains text and the time it should be displayed
 type Lyric struct {
 	Line      string `json:"line"`
 	StartTime int    `json:"start"`
 }
 
+// LyricsConfig contains multiple lyrics in order
 type LyricsConfig struct {
 	Lyrics []Lyric `json:"lyrics"`
 }
 
+// LoadLyrics will load a lyrics file and return a struct for it
 func LoadLyrics(filename string) (LyricsConfig, error) {
+	return loadLyricsImpl(filename, &readJSON{})
+}
+
+func loadLyricsImpl(filename string, jReader readerJSON) (LyricsConfig, error) {
 	filepath := fmt.Sprintf("lyrics/%s", filename)
 
-	jsonFile, err := os.Open(filepath)
+	jsonFile, err := jReader.Open(filepath)
 	if err != nil {
-		fmt.Println(err)
+		return LyricsConfig{}, err
 	}
 
-	byteValue, err := ioutil.ReadAll(jsonFile)
+	byteValue, err := jReader.ReadAll(jsonFile)
 	if err != nil {
 		return LyricsConfig{}, err
 	}
 
 	var lyricsConfig LyricsConfig
 
-	json.Unmarshal(byteValue, &lyricsConfig)
+	if err := jReader.Unmarshal(byteValue, &lyricsConfig); err != nil {
+		return LyricsConfig{}, err
+	}
+
 	return lyricsConfig, nil
+}
+
+type readJSON struct{}
+
+//go:generate go run github.com/vektra/mockery/cmd/mockery -name readerJSON -inpkg --filename read_json_mock.go
+type readerJSON interface {
+	ReadAll(r io.Reader) ([]byte, error)
+	Open(name string) (*os.File, error)
+	Unmarshal(data []byte, v interface{}) error
+}
+
+func (*readJSON) ReadAll(r io.Reader) ([]byte, error) {
+	return ioutil.ReadAll(r)
+}
+
+func (*readJSON) Open(name string) (*os.File, error) {
+	return os.Open(name)
+}
+
+func (*readJSON) Unmarshal(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
 }
